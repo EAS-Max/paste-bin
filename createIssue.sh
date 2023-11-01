b4cfa235b14c5efbaca055889113c2840540a227
EAS-Max <95413284+EAS-Max@users.noreply.github.com>
2023-11-01 15:02:51 +0000

    Create createIssue.sh

---
+++ createIssue.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+TOKEN="YOUR_GITHUB_TOKEN"
+REPO="username/repository"
+TITLE="Issue Title"
+BODY="Issue Description"
+API_URL="https://api.github.com/repos/$REPO/issues"
+
+# Create Issue
+create_issue() {
+    curl -s -X POST "$API_URL" \
+    -H "Authorization: token $TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "title": "'"$TITLE"'",
+        "body": "'"$BODY"'"
+    }'
+}
+
+# Execute the function
+create_issue
